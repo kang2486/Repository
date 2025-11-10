a757bddc4ee4642368c61c8a2512ef628957d479
kang2486 <41343122@nfu.edu.tw>
2025-11-10 17:14:29 +0800

    update

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w,w2;
     w.move(200,100);
-    w2.move(500,100);
+    w2.move(600,100);
 
     w.show();
     w2.show();
