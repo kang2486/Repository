2d15c5fbc48d3764c111d07c6c8a3b710c74b644
kang2486 <41343122@nfu.edu.tw>
2025-11-10 16:43:48 +0800

    123

--- main.cpp
+++ main.cpp
@@ -5,7 +5,11 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    MainWindow w,w2;
+    w.move(200,100);
+    w2.move(400,100);
+
     w.show();
+    w2.show();
     return a.exec();
 }
